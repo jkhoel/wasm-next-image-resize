8c7afd10fa7214dea55db27446e070484578f40b
Jens Kristian Hoel <jkhoel@users.noreply.github.com>
2023-05-14 22:09:12 +0200

    Readme updates and code cleanup (#1)
    * updated note on context
    
    * removed unused code

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,16 +4,6 @@ use fast_image_resize as fr;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn sum(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
-}
-
-#[wasm_bindgen]
 pub fn resize_image(raw_image_width: u32, raw_image_height: u32, buffer: &[u8]) -> Vec<u8> {
     let src_image = fr::Image::from_vec_u8(
         NonZeroU32::new(raw_image_width).unwrap(),
@@ -26,26 +16,4 @@ pub fn resize_image(raw_image_width: u32, raw_image_height: u32, buffer: &[u8]) 
     // for now, just spits the same image back again. TODO: Implement resizing!
 
     src_image.into_vec()
-}
-
-// #[wasm_bindgen]
-// pub fn process_image(raw_image_width: u32, raw_image_height: u32, buffer: &[u8]) -> Vec<u8> {
-//     return Vec::new();
-// }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_can_add() {
-        let result = sum(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn it_can_greet() {
-        let result = greet("Bob");
-        assert_eq!(result, "Hello, Bob!");
-    }
 }
